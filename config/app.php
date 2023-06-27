11480f874d3c48aa0bfedfb42e3ffc8d34e25533
Edson Estevao <edson.n.estevao@gmail.com>
2023-06-26 21:44:08 -0400

    env

--- config/app.php
+++ config/app.php
@@ -67,9 +67,11 @@ return [
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
     |
-    */
+    *
 
     'timezone' => 'UTC',
+    */
+    'timezone' => 'America/Porto_Velho',
 
     /*
     |--------------------------------------------------------------------------
@@ -83,6 +85,7 @@ return [
     */
 
     'locale' => 'en',
+    // 'locale' => 'pt_BR',
 
     /*
     |--------------------------------------------------------------------------
